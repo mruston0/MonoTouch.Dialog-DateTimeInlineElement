1580b3bfe9ecb640a04eb247a736c0518edb0d2a
Matthew Ruston <artanis0@gmail.com>
2014-03-18 13:31:47 -0400

    Addition of DateTimeInlineElement.cs class

---
+++ DateTimeInlineElement.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MonoTouch.Dialog.Extensions
+{
+	public class DateTimeInlineElement : DateTimeElement
+	{
+		bool isPickerPresent = false;
+		UIViewElement datePickerContainer;
+		// We want to hide the DateSelected event in the base class since it is not being used in this implementation.
+		public new event Action DateSelected;
+
+		public DateTimeInlineElement(string caption, DateTime date)
+			: base(caption, date)
+		{
+			MinuteInterval = 5;
+		}
+
+		public override void Selected(DialogViewController dvc, UITableView tableView, NSIndexPath path)
+		{
+			if(datePicker == null)
+			{
+				datePicker = CreatePicker();
+
+				// Whenever the DatePicker is changed, get the DateTimeElement cell and change the text.
+				datePicker.ValueChanged += (object sender, EventArgs e) =>
+				{
+					this.DateValue = datePicker.Date;
+					var cell = tableView.CellAt(path);
+					cell.DetailTextLabel.Text = FormatDate(datePicker.Date);
+					if(DateSelected != null)		// Fire our changed event.
+						DateSelected();
+				};
+			}
+
+			TogglePicker(dvc, tableView, path);
+
+			tableView.DeselectRow(path, true);		// Deselect the row so the row highlint tint fades away.
+		}
+
+		private void TogglePicker(DialogViewController dvc, UITableView tableView, NSIndexPath path)
+		{
+			var sectionAndIndex = GetMySectionAndIndex(dvc);
+			if(sectionAndIndex.Key != null)
+			{
+				Section section = sectionAndIndex.Key;
+				int index = sectionAndIndex.Value;
+
+				var cell = tableView.CellAt(path);
+
+				if(isPickerPresent)
+				{
+					// Remove the picker.
+					cell.DetailTextLabel.TextColor = UIColor.Gray;
+					section.Remove(datePickerContainer);
+					isPickerPresent = false;
+				} else
+				{
+					// Show the picker.
+					cell.DetailTextLabel.TextColor = UIColor.Red;
+					datePickerContainer = new UIViewElement(string.Empty, datePicker, false);
+					section.Insert(index + 1, UITableViewRowAnimation.Bottom, datePickerContainer);
+					isPickerPresent = true;
+				}
+			}
+		}
+
+		private KeyValuePair<Section, int> GetMySectionAndIndex(DialogViewController dvc)
+		{
+			foreach(var section in dvc.Root)
+			{
+				for(int i = 0; i < section.Count; i++)
+				{
+					if(section[i] == this)
+					{
+						return new KeyValuePair<Section, int>(section, i);
+					}
+				}
+			}
+			return new KeyValuePair<Section, int>();
+		}
+	}
+}
+
